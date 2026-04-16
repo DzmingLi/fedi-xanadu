43322d74ceff0d1f42aea6aa190c48aae5e3a493
Dzming Li <i@dzming.li>
2026-04-16 14:31:20 +0800

    Revert "fix: navbar always full width regardless of page layout"
    This reverts commit 50eebc7b13cf7517f6ee58b5804b4afb513219dd.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -314,6 +314,8 @@
     overflow: hidden;
   }
   .profile-nav {
+    max-width: 1080px;
+    margin: 0 auto;
     padding: 0 1rem;
   }
   .profile-container {
@@ -322,6 +324,8 @@
     padding: 0 1rem;
   }
   .top-nav {
+    max-width: 760px;
+    margin: 0 auto;
     padding: 0 1rem;
   }
   .fullwidth-nav {
